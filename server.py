882901c95e82aee8b3fe526e9310434ad7032d70
Eric Lee <ericlee123@gmail.com>
2018-02-24 18:21:26 -0600

    thrift nonsense

--- server.py
+++ server.py
@@ -3,5 +3,7 @@
 class Server:
 
     def __init__(self):
+        self.kv_store = {}
         self.numServers = 0 # this should be initialized after talking to every other server
-        self.kv_store = {} # key -> version -> value
+        self.reachable = list()
+        self.ts = 0
